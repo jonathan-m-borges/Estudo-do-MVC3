002840d1b095a5420e48d266057321a406112ddc
Jonathan Borges <jonathan@inventti.com.br>
2011-09-27 18:01:50 -0300

    Mudança para Ajax (parcial)

---
+++ AdventureWorks_MVC/Scripts/grid.js
@@ -0,0 +1,40 @@
+﻿/// <reference path="jquery-1.6.4-vsdoc.js" />
+/// <reference path="jquery-ui-1.8.16.js" />
+
+var Grid = {
+    UrlController: '',
+    TableId: '',
+    SeletorTabela: '#grid',
+
+    Iniciar: function (urlController) {
+        var that = this;
+        that.UrlController = urlController;
+    },
+
+    CarregarLista: function () {
+        var that = this;
+        $.ajax({
+            type: 'GET',
+            url: that.UrlController + "/Lista",
+            cache: false,
+            dataType: 'json',
+            success: function (data) {
+                $(that.SeletorTabela + ' tbody').empty();
+                $.each(data.Lista, function (i, item) {
+                    var css = (i % 2 == 0) ? "grid_cte_list_line1" : "grid_cte_list_line2";
+                    var linhas = that.CriarLinha(item, css);
+                    if ($.isArray(linhas))
+                        $.each(linhas, function (i, linha) {
+                            tbody.append(linha);
+                        });
+                    else
+                        tbody.append(linhas);
+                });
+            }
+        });
+    },
+
+    CriarLinha: function () {
+    }
+
+}
